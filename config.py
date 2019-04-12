1a4d26ab63ebe6ffa9f1b1a0e5bd3790a11bc913
逸繁 <37973844+songyifan427@users.noreply.github.com>
2019-04-12 10:52:27 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
+#本机字体样式ttf文件绝对路径
 ttfurl='C:\WINDOWS\Fonts\ARVO-REGULAR.TTF'
+#session_key
 secret_key='123456'
+#mysql_host
 mysqlhost="localhost"
+#mysql_user
 user='root'
+#mysql_password
 password='123456'
+#mysql_db
 dbname= 'hisexcel'
