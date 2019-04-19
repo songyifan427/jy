6614c1372c89587df72056afa65ee977f40e58ea
songyifan427 <songyifan427@163.com>
2019-04-19 18:05:30 +0800

    4.19

--- app.py
+++ app.py
@@ -446,4 +446,4 @@ def checkcon():
         return "fail"    
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0",port=5000)
--- config.py
+++ config.py
@@ -5,15 +5,15 @@ ttfurl='.\Arvo-Regular.ttf'
 secret_key='123456'
 #mysql_host
 mysqlhost="172.17.0.4"
-
+# mysqlhost="localhost"
 #mysql_port
 mysqlport=3689
-
+# mysqlport=3306
 #mysql_user
 user='python'
-
+# user='root'
 #mysql_password
 password='RolzO%j4#$yee%LIOJ'
-
+# password='123456'
 #mysql_db
 dbname= 'hisexcel'
