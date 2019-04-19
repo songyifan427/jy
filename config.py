d91f545757e09ff6e14fc6c29a79a2384d7cab03
songyifan427 <songyifan427@163.com>
2019-04-19 18:25:18 +0800

    4.19x2

--- config.py
+++ config.py
@@ -4,16 +4,16 @@ ttfurl='.\Arvo-Regular.ttf'
 #session_key
 secret_key='123456'
 #mysql_host
-mysqlhost="172.17.0.4"
-# mysqlhost="localhost"
+# mysqlhost="172.17.0.4"
+mysqlhost="localhost"
 #mysql_port
-mysqlport=3689
-# mysqlport=3306
+# mysqlport=3689
+mysqlport=3306
 #mysql_user
-user='python'
-# user='root'
+# user='python'
+user='root'
 #mysql_password
-password='RolzO%j4#$yee%LIOJ'
-# password='123456'
+# password='RolzO%j4#$yee%LIOJ'
+password='123456'
 #mysql_db
 dbname= 'hisexcel'
